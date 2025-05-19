93a27be99adfeb722c9ea4bf81bcd8b54f6a905e
randesu <androinimesubs@gmail.com>
2025-05-19 14:24:14 +0700

    update 48

---
+++ app/Livewire/Account/MyOrders/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Account\MyOrders;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.account.my-orders.index');
+    }
+}
---
+++ app/Livewire/Account/MyOrders/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Account\MyOrders;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.account.my-orders.show');
+    }
+}
