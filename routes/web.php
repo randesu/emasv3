86aade1ee5e33856dcb00f10c1d388854eae09c4
sandyti <sandytriantono816@gmail.com>
2025-05-19 14:34:27 +0700

    costumer

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ Route::get('/', function () {
 Route::get('/register', Register::class)->name('register');
 
 Route::get('/login', Auth\Login::class)->name('login');
+
+Route::middleware('auth:customer')->group(function () {
+    
+    Route::group(['prefix' => 'account'], function () {
+        
+        //route my order
+        Route::get('/my-orders', Account\MyOrders\Index::class)->name('account.my-orders.index');
+  
+        //route my order show
+        Route::get('/my-orders/{snap_token}', Account\MyOrders\Show::class)->name('account.my-orders.show');
+
+    });
+
+});
