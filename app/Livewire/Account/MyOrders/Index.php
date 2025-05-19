3af16dfaf072914537b619fe6a2b0cd186e94553
salsabila <salsabilla160504@gmail.com>
2025-05-19 14:42:05 +0700

    index my order

--- app/Livewire/Account/MyOrders/Index.php
+++ app/Livewire/Account/MyOrders/Index.php
@@ -3,11 +3,18 @@
 namespace App\Livewire\Account\MyOrders;
 
 use Livewire\Component;
+use App\Models\Transaction;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.account.my-orders.index');
+        //get transactions
+        $transactions = Transaction::query()
+            ->where('customer_id', auth()->guard('customer')->user()->id)
+            ->latest()
+            ->simplePaginate(3);
+
+        return view('livewire.account.my-orders.index', compact('transactions'));
     }
 }
