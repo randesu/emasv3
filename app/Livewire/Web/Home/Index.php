7c41a4f71f5bf46e12a3efba3924092528404bd7
sandyti <sandytriantono816@gmail.com>
2025-05-20 10:45:42 +0700

    update produk

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Web\Home;
 
-use App\Models\herobanner;
+use App\Models\produk;
 use Livewire\Component;
 use App\Models\Category;
+use App\Models\herobanner;
 
 class Index extends Component
 {
+     protected function getLatestProducts()
+    {
+        //get products
+        return produk::query()
+            // ->with('category', 'ratings.customer')
+            // ->withAvg('ratings', 'rating')
+            ->limit(5)
+            ->orderBy('created_at')
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.web.home.index', [
@@ -17,6 +29,9 @@ class Index extends Component
             
              //get categories
             'categories' => Category::latest()->get(),
+
+             //get latest products
+            'latestProducts' => $this->getLatestProducts(),
 
         ]);
     }
