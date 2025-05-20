27f417c9933f8b8eb7503336018e9ef10327e7ff
randesu <androinimesubs@gmail.com>
2025-05-20 10:06:01 +0700

    update gambar kategory

--- resources/views/components/cards/category.blade.php
+++ resources/views/components/cards/category.blade.php
@@ -2,7 +2,7 @@
     <a href="/category/{{ $category->slug }}" wire:navigate class="text-decoration-none">
         <div class="card border-0 rounded shadow-sm">
             <div class="card-body text-center">
-                <img src="{{ asset('/storage/' . $category->image) }}" class="img-fluid" width="50">
+                <img src="{{ asset( $category->imagelink) }}" class="img-fluid" width="50">
                 <label class="text-center">{{ $category->name }}</label>
             </div>
         </div>
