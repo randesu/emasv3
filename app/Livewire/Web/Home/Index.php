8b4fb739ed6ac898d6ad5eda1a3f7a930e6ee8c8
salsabila <salsabilla160504@gmail.com>
2025-05-20 09:08:45 +0700

    Menampilkan Data Slider Done

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\Web\Home;
 
+use App\Models\Slider;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.web.home.index');
+        return view('livewire.web.home.index', [
+
+            //get sliders
+            'sliders' => Slider::latest()->get(),
+            
+        ]);
     }
 }
